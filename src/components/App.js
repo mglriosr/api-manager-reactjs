894afd20b75df9b64b2e91e3fbe142382426a1c8
Miguel Rios R <mgl.rios.r@gmail.com>
2020-01-28 00:23:41 -0500

    Init api geter

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Home from '../pages/Home'
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+  state = ({
+    loading: true,
+    error: null,
+    data: []
+  })
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+  async fetchData(){
+    this.setState({loading: true, error: null})
+
+    try {
+      var data;
+      var response = await fetch('https://jsonplaceholder.typicode.com/users');
+      data = await response.json()
+      this.setState({loading: false, data: data})
+    } catch (error) {
+      this.setState({loading: false, error: error})
+    }
+
+
+  }
+
+  render(){
+    console.log(this.state.data)
+    if ( this.state.loading === true ) {
+      return <p>Cargando...</p>
+    }
+
+    if ( this.state.error ) {
+      return <p>Ocurrió un error :(</p>
+    }
+
+    return this.state.data.map((item) =>
+      <li key={item.id}>{item.name}</li>
+    );
+  }
+}
+
+export default Home;
